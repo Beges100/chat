e40460f4c40b9ecb87089a931afb5fd5d13f570d
Maximus <naturemade@yandex.ru>
2024-03-26 21:47:07 +0300

    Package + gitignore

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -3,36 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <h1>Hello world</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <h1>Hello world</h1>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 
-  async function F() {
-    let response = await fetch("http://localhost:8080/get");
+    async function F() {
+        let response = await fetch("http://localhost:8080/get");
 
-    if (response.ok) { // если HTTP-статус в диапазоне 200-299
-      // получаем тело ответа (см. про этот метод ниже)
-      let json = await response.json();
-    } else {
-      alert("Ошибка HTTP: " + response.status);
+        if (response.ok) { // если HTTP-статус в диапазоне 200-299
+            // получаем тело ответа (см. про этот метод ниже)
+            let json = await response.json();
+        } else {
+            alert("Ошибка HTTP: " + response.status);
+        }
     }
-  }
 }
 
 export default App;
