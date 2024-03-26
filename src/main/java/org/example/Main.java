55764bce0f9319977fc20b76a512d960d6935959
Maximus <naturemade@yandex.ru>
2024-03-26 21:59:22 +0300

    Котлин + евент

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,21 @@
 package org.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
 
+@Slf4j
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
+
+    }
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void appStated() {
+        log.info("Приложение chat стартовало");
     }
 }
